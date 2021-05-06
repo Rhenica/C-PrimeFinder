58885b6916bf6ae2460ea3e3f9bbfce96648645a
Rhenica <83538157+Rhenica@users.noreply.github.com>
2021-05-06 20:49:59 +0300

    Update prime.c

--- prime.c
+++ prime.c
@@ -2,9 +2,7 @@
 #include <stdbool.h>
 
 bool isPrime(int n) {
-    if (n % 2 == 0) return false;
-    if (n % 3 == 0) return false;
-    if (n % 5 == 0) return false;
+    if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0) return false;
     for (int i = 5; i < n/2; i += 2) if (n % i == 0) return false;
     return true;
 }
